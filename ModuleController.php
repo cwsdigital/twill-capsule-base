9c943fc420065905de9f4c26c155125125976073
Chris Pymm <chris@chrispymm.co.uk>
2021-09-30 13:46:26 +0100

    hide locale in url slug if only one locale

--- ModuleController.php
+++ ModuleController.php
@@ -5,6 +5,7 @@ namespace App\Twill\Capsules\Base;
 use Illuminate\Http\Request;
 use Illuminate\Contracts\Foundation\Application;
 use A17\Twill\Http\Controllers\Admin\ModuleController as TwillModuleController;
+use Illuminate\Support\Facades\Config;
 
 class ModuleController extends TwillModuleController
 {
@@ -13,5 +14,20 @@ class ModuleController extends TwillModuleController
         parent::__construct($app, $request);
 
         $this->defaultOrders = [$this->titleColumnKey => 'asc'];
+    }
+
+    protected function getPermalinkBaseUrl()
+    {
+        $appUrl = Config::get('app.url');
+
+        if (blank(parse_url($appUrl)['scheme'] ?? null)) {
+            $appUrl = $this->request->getScheme() . '://' . $appUrl;
+        }
+
+        return $appUrl . '/'
+            . ($this->moduleHas('translations') && (count( config('translatable.locales') ) > 1) ? '{language}/' : '')
+                . ($this->moduleHas('revisions') ? '{preview}/' : '')
+                . ($this->permalinkBase ?? $this->getModulePermalinkBase())
+                . (isset($this->permalinkBase) && empty($this->permalinkBase) ? '' : '/');
     }
 }
